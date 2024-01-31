8e5f425c9f7adab0314b2b5f4199d31e58459a73
Ishita Hardasmalani <hardasmalaniishita@gmail.com>
2024-01-31 14:49:19 +0530

    add images, home page create

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage.jsx"
-import RegistrationForm from "./pages/RegistrationForm.jsx"
 import Header from "./components/Header.jsx";
+import Body from "./components/Body.jsx"
+import Footer from "./components/Footer.jsx"
 const App = () => {
   return <div>
     {/* <LoginPage/> */}
     {/* <RegistrationForm/> */}
-     {/* <Header/> */}
+     <Header/>
+     <Body/>
+     <Footer/>
   </div>;
 };
 
