18083ad8f67efe7aeb5eae8f662753ffed1ceb71
Ishita Hardasmalani <hardasmalaniishita@gmail.com>
2024-01-31 08:47:45 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
-  return <div>App</div>;
+  return <div>
+    <Homepage/>
+  </div>;
 };
 
 export default App;
---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div className='text-4xl text-red-900'>
+      Homepage
+    </div>
+  )
+}
+
+export default Homepage
