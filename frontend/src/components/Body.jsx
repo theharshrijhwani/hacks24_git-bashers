60fad6793640487b0322743d5284f259a9691fcd
theharshrijhwani <110522542+theharshrijhwani@users.noreply.github.com>
2024-01-31 16:03:17 +0530

    landpage done

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -2,24 +2,25 @@
 import React from "react";
 
 import home from "../assets/home.png";
-import apartment1 from "../assets/apartment1.png"
-import apartment2 from "../assets/apartment2.png"
-import apartment3 from "../assets/apartment3.png"
-import cafe1 from "../assets/cafe1.png"
-import cafe2 from "../assets/cafe2.png"
-import cafe3 from "../assets/cafe3.png"
-import classroom1 from "../assets/classroom1.png"
-import classroom2 from "../assets/classroom2.png"
-import classroom3 from "../assets/classroom3.png"
-import kitchen1 from "../assets/kitchen1.png"
-import kitchen2 from "../assets/kitchen2.png"
-import kitchen3 from "../assets/kitchen3.png"
-import office1 from "../assets/office1.png"
-import office2 from "../assets/office2.png"
-import office3 from "../assets/office3.png"
-import restaurant1 from "../assets/restaurant1.png"
-import restaurant2 from "../assets/restaurant2.png"
-import restaurant3 from "../assets/restaurant3.png"
+import apartment1 from "../assets/apartment1.png";
+import apartment2 from "../assets/apartment2.png";
+import apartment3 from "../assets/apartment3.png";
+import cafe1 from "../assets/cafe1.png";
+import cafe2 from "../assets/cafe2.png";
+import cafe3 from "../assets/cafe3.png";
+import classroom1 from "../assets/classroom1.png";
+import classroom2 from "../assets/classroom2.png";
+import classroom3 from "../assets/classroom3.png";
+import kitchen1 from "../assets/kitchen1.png";
+import kitchen2 from "../assets/kitchen2.png";
+import kitchen3 from "../assets/kitchen3.png";
+import office1 from "../assets/office1.png";
+import office2 from "../assets/office2.png";
+import office3 from "../assets/office3.png";
+import restaurant1 from "../assets/restaurant1.png";
+import restaurant2 from "../assets/restaurant2.png";
+import restaurant3 from "../assets/restaurant3.png";
+import market_place from "../assets/market_place.png";
 
 const Body = () => {
   return (
@@ -66,54 +67,34 @@ const Body = () => {
         </p>
       </div>
       <div>
-        <h2>What can you create with us?</h2>
-        <div>
-            <h2>APARTMENT</h2>
-            <img
-            src={apartment1}
-            alt="Interior Design"
-            className="float-left w-[600px] h-[400px] rounded-[12px] "
-          />
+        <h2 className="mt-5 font-bold text-xl">What can you create with us?</h2>
+      </div>
+      <div className="flex justify-evenly items-center gap-7 mt-5">
+        <div className="flex-1 items-center justify-center flex flex-col">
+          <h1 className="text-[1.2rem]">Restaurant</h1>
+          <img className="mt-3" src={restaurant1} />
         </div>
-        <div>
-            <h2>RESTAURANTS</h2>
-            <img
-            src={restaurant1}
-            alt="Interior Design"
-            className="float-left w-[600px] h-[400px] rounded-[12px] "
-          />
+        <div className="flex-1 items-center justify-center flex flex-col">
+          <h1 className="text-[1.2rem]">Kitchen</h1>
+          <img className="mt-3 object-cover w-full h-full" src={kitchen1} />
         </div>
-        <div>
-            <h2>CAFES</h2>
-            <img
-            src={cafe1}
-            alt="Interior Design"
-            className="float-left w-[600px] h-[400px] rounded-[12px] "
-          />
+        <div className="flex-1 items-center justify-center flex flex-col">
+          <h1 className="text-[1.2rem]">Cafe</h1>
+          <img className="mt-3 object-cover w-full h-full" src={cafe1} />
         </div>
-        <div>
-            <h2>KITCHEN</h2>
-            <img
-            src={kitchen1}
-            alt="Interior Design"
-            className="float-left w-[600px] h-[400px] rounded-[12px] "
-          />
+      </div>
+      <div className="flex justify-evenly items-center mt-5 gap-7">
+        <div className="flex-1 items-center justify-center flex flex-col">
+          <h1 className="text-[1.2rem]">Classrooms</h1>
+          <img className="mt-3" src={classroom1} />
         </div>
-        <div>
-            <h2>OFFICE</h2>
-            <img
-            src={office1}
-            alt="Interior Design"
-            className="float-left w-[600px] h-[400px] rounded-[12px] "
-          />
+        <div className="flex-1 items-center justify-center flex flex-col">
+          <h1 className="text-[1.2rem]">Apartments</h1>
+          <img className="mt-3 object-cover w-full h-full" src={apartment1} />
         </div>
-        <div>
-            <h2>CLASSROOM</h2>
-            <img
-            src={classroom1}
-            alt="Interior Design"
-            className="float-left w-[600px] h-[400px] rounded-[12px] "
-          />
+        <div className="flex-1 items-center justify-center flex flex-col">
+          <h1 className="text-[1.2rem]">Office</h1>
+          <img className="mt-3" src={office1} />
         </div>
       </div>
     </div>
