6032573c7c68ee06901b09baa445692641ea1bb2
theharshrijhwani <110522542+theharshrijhwani@users.noreply.github.com>
2024-01-31 19:52:25 +0530

    first routing

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -59,9 +59,13 @@ const SignUpPage = () => {
     <div className="h-screen md:flex">
       <div className="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 justify-around items-center hidden">
         <div>
-          <h1 className="text-white font-bold text-4xl font-sans">UrbanSpaces</h1>
-          <p className="text-white mt-1">From conceptualization to implementation, we bring your
-            vision to life.</p>
+          <h1 className="text-white font-bold text-4xl font-sans">
+            UrbanSpaces
+          </h1>
+          <p className="text-white mt-1">
+            From conceptualization to implementation, we bring your vision to
+            life.
+          </p>
           {/* <button type="submit" className="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2">Read More</button> */}
         </div>
         <div className="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
@@ -74,9 +78,9 @@ const SignUpPage = () => {
           <h2 className="text-3xl font-semibold mb-4">Sign Up</h2>
           <div className="mb-4">
             <label
-            htmlFor="firstName"
-            className="block text-sm font-medium text-gray-600"
-          >
+              htmlFor="firstName"
+              className="block text-sm font-medium text-gray-600"
+            >
               First Name
             </label>
             <input
@@ -91,9 +95,9 @@ const SignUpPage = () => {
           </div>
           <div className="mb-4">
             <label
-            htmlFor="lastName"
-            className="block text-sm font-medium text-gray-600"
-          >
+              htmlFor="lastName"
+              className="block text-sm font-medium text-gray-600"
+            >
               Last Name
             </label>
             <input
@@ -108,9 +112,9 @@ const SignUpPage = () => {
           </div>
           <div className="mb-4">
             <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-600"
-          >
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-600"
+            >
               Email
             </label>
             <input
@@ -125,9 +129,9 @@ const SignUpPage = () => {
           </div>
           <div className="mb-4 relative">
             <label
-            htmlFor="password"
-            className="block text-sm font-medium text-gray-600"
-          >
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-600"
+            >
               Password
             </label>
             <input
@@ -149,9 +153,9 @@ const SignUpPage = () => {
           </div>
           <div className="mb-4">
             <label
-            htmlFor="userType"
-            className="block text-sm font-medium text-gray-600"
-          >
+              htmlFor="userType"
+              className="block text-sm font-medium text-gray-600"
+            >
               Type
             </label>
             <select
@@ -167,12 +171,14 @@ const SignUpPage = () => {
               <option value="Client">Client</option>
             </select>
           </div>
-          <button
-            type="submit"
-            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none"
-          >
-            Submit
-          </button>
+          <Link to="/signin">
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none"
+            >
+              Submit
+            </button>
+          </Link>
         </form>
       </div>
     </div>
